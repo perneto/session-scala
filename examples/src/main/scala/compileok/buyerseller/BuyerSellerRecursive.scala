8f3b55aa9a051aa7341bf425e28c6e45e136ff6f
Olivier Pernet <o.pernet@gmail.com>
2010-12-02 01:34:23 +0000

    changed recursive example temporarily, need to figure out how to deal with scopes and nested functions

--- examples/src/main/scala/compileok/buyerseller/BuyerSellerRecursive.scala
+++ examples/src/main/scala/compileok/buyerseller/BuyerSellerRecursive.scala
@@ -10,42 +10,43 @@ object BuyerSellerRecursive {
     val sharedChannel = SharedChannel.createLocalChannel(Set('Buyer, 'Seller))
 
     actor {
+      def quoteRecursion(s: SessionChannel, quote: Int) {
+        s('Buyer) ! quote
+        s('Buyer).receive {
+          case OK =>
+            s('Buyer) ! new Invoice(quote)
+            val payment = s('Buyer).?[Payment]
+          case NotOK =>
+            val reason = s('Buyer).?[String]
+            quoteRecursion(s, quote - 100)
+        }
+      }
+
       sharedChannel.join('Seller) { s =>
         println("Seller: started")
         val o = s('Buyer).?[Order]
-        def quoteRecursion(s: SessionChannel, quote: Int) {
-          s('Buyer) ! quote
-          s('Buyer).receive {
-            case OK =>
-              s('Buyer) ! new Invoice(quote)
-              val payment = s('Buyer).?[Payment]
-            case NotOK =>
-              val reason = s('Buyer).?[String]
-              quoteRecursion(s, quote - 100)
-          }
-        }
         quoteRecursion(s, 2000)
-
         println("Seller: finished")
       }
     }
 
     actor {
+      def quoteRecursion(s: SessionChannel) {
+        val price = s('Seller).?[Int]
+        if (price < 1950) {
+          s('Seller) ! OK
+          val invoice = s('Seller).?[Invoice]
+          s('Seller) ! new Payment(price)
+        } else {
+          s('Seller) ! NotOK
+          s('Seller) ! "Too expensive"
+          quoteRecursion(s)
+        }
+      }
+
       sharedChannel.join('Buyer) { s =>
         println("Buyer: started")
         s('Seller) ! new Order(100)
-        def quoteRecursion(s: SessionChannel) {
-          val price = s('Seller).?[Int]
-          if (price < 1950) {
-            s('Seller) ! OK
-            val invoice = s('Seller).?[Invoice]
-            s('Seller) ! new Payment(price)
-          } else {
-            s('Seller) ! NotOK
-            s('Seller) ! "Too expensive"
-            quoteRecursion(s)
-          }
-        }
         quoteRecursion(s)
         println("Buyer: finished")
       }
