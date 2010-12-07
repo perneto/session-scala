5f27804326c0f449024d0c04fedab40e3458911b
Olivier Pernet <o.pernet@gmail.com>
2010-12-07 20:26:00 +0000

    push.sh, added monitor machines

--- examples/src/main/resources/benchmarks/push.sh
+++ examples/src/main/resources/benchmarks/push.sh
@@ -3,3 +3,6 @@
 hg push ssh://shade02/session-scala
 hg push ssh://shadexx/session-scala
 hg push ssh://shade04/session-scala
+hg push ssh://treebeard/session-scala
+hg push ssh://matrix13/session-scala
+hg push ssh://matrix16/session-scala
