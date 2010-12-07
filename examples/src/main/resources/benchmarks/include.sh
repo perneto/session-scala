40991a14b36a4ec1c542d8bd11dcedfc079fd337
Olivier Pernet <o.pernet@gmail.com>
2010-12-07 20:07:16 +0000

    tweak to hg pull in script

--- examples/src/main/resources/benchmarks/include.sh
+++ examples/src/main/resources/benchmarks/include.sh
@@ -8,7 +8,7 @@ sessml_update() {
    (cd SessML; svn up && make)
 }
 
-BENCHMARKS=session-scala/examples/src/main/resources/benchmarks
+BENCHMARKS=~/session-scala/examples/src/main/resources/benchmarks
 RT_LIB_DIR=session-scala/runtime/lib_managed/2.8.1/compile/
 RUNTIME_LIB=$RT_LIB_DIR/amqp-client-2.1.1.jar:$RT_LIB_DIR/commons-cli-1.1.jar:$RT_LIB_DIR/commons-io-1.2.jar:$RT_LIB_DIR/scalaj-collection_2.8.0-1.0.jar
 RUNTIME_JAR=session-scala/runtime/target/scala_2.8.1/runtime_2.8.1-0.1.jar
@@ -18,7 +18,7 @@ EXAMPLES_JAR=session-scala/examples/target/scala_2.8.1/examples_2.8.1-0.1.jar
 # $2: IP address of monitor (or AMQP broker)
 start_role() {
   cd session-scala
-  hg pull -u
+  hg pull -u https://session-scala.googlecode.com/hg/
   sbt package
   scala -cp $RUNTIME_LIB:$RUNTIME_JAR:$EXAMPLES_JAR benchmark.buyerseller.$1 $2
 }
