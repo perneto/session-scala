f0a96388bed41b3bbcd2bb847b3e0ba513552824
Olivier Pernet <o.pernet@gmail.com>
2010-12-13 19:40:23 +0000

    more logging

--- examples/src/main/resources/benchmarks/include.sh
+++ examples/src/main/resources/benchmarks/include.sh
@@ -16,4 +16,5 @@ start_role() {
   sbt "project runtime" update package "project examples" package
   echo scala -cp $RUNTIME_LIB:$RUNTIME_JAR:$EXAMPLES_JAR benchmark.buyerseller.$1 $2
   scala -cp $RUNTIME_LIB:$RUNTIME_JAR:$EXAMPLES_JAR benchmark.buyerseller.$1 $2
+  echo "Client role $1 : scala exited" 
 }
