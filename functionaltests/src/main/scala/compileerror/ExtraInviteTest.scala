31afeba5c510c2c8d8c3063400fe558dfb1968d5
Olivier Pernet <o.pernet@gmail.com>
2011-02-07 17:19:23 +0000

    wip: functional test for invites

--- functionaltests/src/main/scala/compileerror/ExtraInviteTest.scala
+++ functionaltests/src/main/scala/compileerror/ExtraInviteTest.scala
@@ -1,8 +1,9 @@
 package compileerror
 
-import uk.ac.ic.doc.sessionscala.{SharedChannel}
+import uk.ac.ic.doc.sessionscala.SharedChannel
+import SharedChannel.localhost
 
-object SessionOperationsInClosureTest {
+object ExtraInviteTest {
   def main(args: Array[String]) {
     SharedChannel.withLocalChannel(
     """
@@ -14,11 +15,7 @@ object SessionOperationsInClosureTest {
     """
     ) { sharedChannel =>
 
-      sharedChannel.join('Alice) { s =>
-        var x = 42
-        for (i <- 1 to 10) s('Bob) ! "loop"
-        s('Bob).?[Int]
-      }
+      sharedChannel.invite('Alice -> localhost, 'Alice -> localhost)
     }
   }
 }
